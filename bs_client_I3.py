92b7fa26dae6f53602e0f95ee741163bdb88a903
Antoine <ant.reberat@murena.io>
2024-10-11 10:29:44 +0200

    fix

--- bs_client_I3.py
+++ bs_client_I3.py
@@ -4,17 +4,18 @@ host = '10.0.1.12'
 port = 13337
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-val = input("Que veux-tu envoyer au serveur : ")
-
-if type(val) is not str:
-    raise TypeError("Veuillez entrer une string !")
-elif not re.search("^(meo|waf)$", val):
-    raise ValueError("Mauvaise valeur entrée, soit waf soit meo !")
-
 try:
     s.connect((host, port))
     print(f"Connecté avec succès au serveur {host} sur le port {port}")
+
+    val = input("Que veux-tu envoyer au serveur : ")
+
+    if type(val) is not str:
+        raise TypeError("Veuillez entrer une string !")
+    elif not re.search("^(meo|waf)$", val):
+        raise ValueError("Mauvaise valeur entrée, soit waf soit meo !")
+
+
     s.sendall(str.encode(val))
     data = s.recv(1024).decode("utf-8")
 
