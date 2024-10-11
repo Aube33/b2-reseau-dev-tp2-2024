fbb79c16f0dbc33aafb987d47420c1489ca23c9f
Antoine <ant.reberat@murena.io>
2024-10-11 10:12:15 +0200

    fix

--- bs_server_I2.py
+++ bs_server_I2.py
@@ -19,7 +19,7 @@ while True:
         print(f"Un client vient de se co et son IP c'est {client_ip}.")
 
         if "meo" in data:
-            conn.sendall(b"Meo à toi confrère.".encode("utf-8"))
+            conn.sendall(str.encode("Meo à toi confrère.", "utf-8"))
         elif "waf" in data:
             conn.sendall(b"ptdr t ki")
         else:
