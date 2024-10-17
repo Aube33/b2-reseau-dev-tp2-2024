9e73703abbc73ada413b090e38ad2f20694c7f45
reantoine <antoine.reberat@ynov.com>
2024-10-17 11:34:26 +0200

    même nom pour variable et fonction format

--- bs_client_II2B.py
+++ bs_client_II2B.py
@@ -8,14 +8,14 @@ class CustomFormatter(logging.Formatter):
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
-    format = "%(levelname)s %(message)s"
+    log_format = "%(levelname)s %(message)s"
 
     FORMATS = {
-        logging.DEBUG: format,
-        logging.INFO: format,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
+        logging.DEBUG: log_format,
+        logging.INFO: log_format,
+        logging.WARNING: yellow + log_format + reset,
+        logging.ERROR: red + log_format + reset,
+        logging.CRITICAL: bold_red + log_format + reset
     }
 
     def format(self, record):
